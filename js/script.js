3a9fd75f16dba2c385262239e5a8d4f8e3c6d4dc
millooo94 <camillomusmeciunict@gmail.com>
2022-10-20 15:51:42 +0200

    Refactor all

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ const price = Number(distance) * 0.21
 if (age < 18) {
     let discount = price * 0.2
     let newprice = price - discount
-    console.log ('È stato applicato uno sconto del 20%! Il suo prezzo è:', newprice.toFixed,"€");
+    console.log ('È stato applicato uno sconto del 20%! Il suo prezzo è:', newprice.toFixed(2),"€");
 } else if (age > 65) {
     let discount = price * 0.4
     let newprice = price - discount
-    console.log ('È stato applicato uno sconto del 40%! Il suo prezzo è:', newprice.toFixed,"€");
+    console.log ('È stato applicato uno sconto del 40%! Il suo prezzo è:', newprice.toFixed(2),"€");
 } else {
     console.log ('Non è stato applicato alcuno sconto. Il suo prezzo è:', price.toFixed(2),"€");
 }
